606d51e748ee2ad2f5c212385b8b55589e6fa7f6
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2025-03-05 09:05:35 -0600

    Finished weapon distributions

--- 42/media/lua/server/items/robloxWeaponDistributions.lua
+++ 42/media/lua/server/items/robloxWeaponDistributions.lua
@@ -1,4 +1,5 @@
 require 'Items/ProceduralDistributions'
+require 'Items/SuburbsDistributions'
 
 -- ["Linked Sword"]
 table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
@@ -15,6 +16,11 @@ table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, "Junkie
 table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, 1);
 table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
 table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, 1);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "JunkiezMoreSupplements.JunkiezLinkedSword");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.01);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "JunkiezMoreSupplements.JunkiezLinkedSword");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.01);
+
 -- ["Paintball Gun"]
 table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
 table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, 6);
@@ -24,9 +30,35 @@ table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, "JunkiezRob
 table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, 2);
 table.insert(ProceduralDistributions["list"]["Hobbies"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
 table.insert(ProceduralDistributions["list"]["Hobbies"].items, 0.5);
-table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
-table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, 0.5);
-table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
-table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, 1);
-table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
-table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, 1);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "JunkiezMoreSupplements.JunkiezPaintballGun");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.01);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "JunkiezMoreSupplements.JunkiezPaintballGun");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.01);
+
+-- ["Paintball Magazine"]
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezPaintballMagazine");
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, 8);
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, "JunkiezRobloxWeapons.JunkiezPaintballMagazine");
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, 5);
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, "JunkiezRobloxWeapons.JunkiezPaintballMagazine");
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, 2);
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, "JunkiezRobloxWeapons.JunkiezPaintballMagazine");
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, 1.0);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "JunkiezMoreSupplements.JunkiezPaintballMagazine");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.01);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "JunkiezMoreSupplements.JunkiezPaintballMagazine");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.01);
+
+-- ["Paintball Box"]
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezPaintballBox");
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, 8);
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, "JunkiezRobloxWeapons.JunkiezPaintballBox");
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, 5);
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, "JunkiezRobloxWeapons.JunkiezPaintballBox");
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, 2);
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, "JunkiezRobloxWeapons.JunkiezPaintballBox");
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, 1.0);
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, "JunkiezMoreSupplements.JunkiezPaintballBox");
+table.insert(SuburbsDistributions["all"]["inventorymale"].items, 0.01);
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, "JunkiezMoreSupplements.JunkiezPaintballBox");
+table.insert(SuburbsDistributions["all"]["inventoryfemale"].items, 0.01);
