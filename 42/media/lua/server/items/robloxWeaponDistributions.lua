dfb487dc6a439612a2f26a6c76b5c40598b2d1a2
James Kelley <77459568+jameskyvo@users.noreply.github.com>
2025-03-04 23:37:34 -0600

    added paintball box and initial distributions

---
+++ 42/media/lua/server/items/robloxWeaponDistributions.lua
@@ -0,0 +1,32 @@
+require 'Items/ProceduralDistributions'
+
+-- ["Linked Sword"]
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, 6);
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, 4);
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, 2);
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, 1);
+table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, "JunkiezRobloxWeapons.JunkiezLinkedSword");
+table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, 1);
+-- ["Paintball Gun"]
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["ArmySurplusTools"].items, 6);
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["ClosetInstruments"].items, 4);
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["CyberCafeDesk"].items, 2);
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["Hobbies"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Mid"].items, 0.5);
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["MeleeWeapons_Late"].items, 1);
+table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, "JunkiezRobloxWeapons.JunkiezPaintballGun");
+table.insert(ProceduralDistributions["list"]["PawnShopKnives"].items, 1);
